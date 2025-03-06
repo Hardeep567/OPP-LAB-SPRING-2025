085ac413c34ef7ac4e7c43e48c9e23978b2072c9
Hardeep567 <hardeepnaresh8@gmail.com>
2025-03-06 05:12:20 -0800

    Update Q2.cpp

--- LAB 5/Q2.cpp
+++ LAB 5/Q2.cpp
@@ -5,29 +5,47 @@ class Student{
     private:
     string id;
     string *name;
+    int nummarks;
     double *marks;
     public:
-    Student(string i,string a,double m){
+    int i=0;
+    Student(string i,string a,int num){
         id = i;
         name = new string;
         *name=a;
-        marks = new double ;
-        *marks = m;
+        nummarks = num;
+        marks = new double [num];
+        
+    }
+    void addmarks(double sorces){
+        if(i > nummarks){
+            cout << " Marks is added completely";
+            return ;
+        }
+        marks[i++] = sorces;
     }
     Student (Student *a){
         id = (a->id);
         name = new string(*a->name);
-        marks = new double(*a->marks);
+        nummarks = a->nummarks;
+        marks = new double [nummarks];
+        i = a->i;
+        for(int j=0;j<i;j++){
+            marks[j] = a->marks[j];
+        }
     }
     void display(){
         cout << " Id : " << id << endl;
         cout << " Name : " << *name << endl;
-        cout << " Marks : " << *marks<< endl; 
+        for(int j=0;j<i;j++){
+        cout << " Marks : " << marks[j]<< endl; 
+        }
     }
 };
 
 int main(){
-    Student a1("101"," hardeep",23.5 );
+    Student a1("101"," hardeep",2);
+    a1.addmarks(23);
     Student a2(&a1);
     a1.display();
     a2.display();
